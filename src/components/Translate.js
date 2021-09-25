33a39e1672bdeb7406248bc8e2a5080fff7eee59
Renato Santana <renatodev@icloud.com>
2021-09-25 18:59:38 -0300

    translator properly working

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -5,33 +5,50 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function Translate() {
-  const [inputText, setInputText] = useState("");
-  const [detectLanguageKey, setdetectedLanguageKey] = useState("");
-  const [selectedLanguageKey, setLanguageKey] = useState("");
+  const [inputText, setInputText] = useState('');
+  const [detectLanguageKey, setdetectedLanguageKey] = useState('');
+  const [selectedLanguageKey, setLanguageKey] = useState('')
   const [languagesList, setLanguagesList] = useState([])
+  const [resultText, setResultText] = useState('');
   const getLanguageSource = () => {
-    axios
-      .post("https://libretranslate.de/detect", {
-        q: inputText,
-      })
-      .then((response) => {
-        setdetectedLanguageKey(response.data[0].language);
-      });
-  };
-  const languageKey = (selectedLanguage) => {
-      setLanguageKey(selectedLanguage.target.value)
-  }
+    axios.post(`https://libretranslate.de/detect`, {
+        q: inputText
+    })
+        .then((response) => {
+            setdetectedLanguageKey(response.data[0].language)
+        })
+}
 
-  useEffect(() => {
-    axios.get(`https://libretranslate.de/languages`).then((response) => {
-      setLanguagesList(response.data);
-    });
-  }, []);
+const translateText = () => {
+  getLanguageSource();
+
+  let data = {
+      q : inputText,
+      source: detectLanguageKey,
+      target: selectedLanguageKey
+  }
+  axios.post(`https://libretranslate.de/translate`, data)
+  .then((response) => {
+      setResultText(response.data.translatedText)
+  })
+}
+
+const languageKey = (selectedLanguage) => {
+  setLanguageKey(selectedLanguage.target.value)
+}
+
+useEffect(() => {
+    axios.get(`https://libretranslate.de/languages`)
+        .then((response) => {
+            setLanguagesList(response.data)
+        })
+    getLanguageSource()
+}, [inputText])
 
   return (
     <div>
       <div className="app-header">
-        <h2 className="header">Best Text Translator</h2>
+        <h2 className="header">Simple Text Translator</h2>
       </div>
 
       <div className="app-body">
@@ -42,21 +59,30 @@ export default function Translate() {
               placeholder="Type Text to Translate.."
               onChange={(e) => setInputText(e.target.value)}
             />
-            <select className="language-select">
-              <option>Please select language</option>
+
+            <select className="language-select" onChange={languageKey}>
+              <option>Please Select Language..</option>
               {languagesList.map((language) => {
-                    return (
-                        <option value={language.code}>
-                            {language.name}
-                        </option>
-                    )
-                })}
+                  return (
+                      <option value={language.code}>
+                          {language.name}
+                      </option>
+                  )
+              })}
             </select>
+
             <Form.Field
               control={TextArea}
               placeholder=" Your Result Translation.."
+              value={resultText}
             />
-            <Button color="orange" size="large" onClick={getLanguageSource}>
+
+            <Button 
+              size="large"
+              color="blue"
+              onClick={translateText}
+            >
+
               <Icon name="translate" />
             </Button>
           </Form>
